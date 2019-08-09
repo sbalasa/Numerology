858ecae12af417b62533ffe45ce198b6eaba2db6
Santhosh Balasa <santhosh.kbr@gmail.com>
2019-08-09 20:17:29 +0530

    Added Exception handling

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from flask import Flask, request, render_template
 from numerology import score, mini_score
 
-app = Flask('app')
+app = Flask("app")
 
 
-@app.route('/')
+@app.route("/")
 def get_values():
     return render_template("get_values.html")
 
 
-@app.route('/results', methods=['POST', 'GET'])
+@app.route("/results", methods=["POST", "GET"])
 def results():
     if request.method == "POST":
-        firstname = request.form["firstname"]
-        middlename = request.form["middlename"]
-        lastname = request.form["lastname"]
-        birthday = request.form["bday"]
-        birthday = birthday.replace('-', "").replace('/', "")
-        bday = birthday[:2]
-        firstname_score = score(firstname)
-        middlename_score = score(middlename)
-        lastname_score = score(lastname)
         try:
-          bday_score = mini_score(bday)
-          birthday_score = mini_score(birthday)
-        except TypeError:
-          bday_score = 0
-          birthday_score = 0
-        final_score = score(firstname + middlename +lastname)
+            firstname = request.form["firstname"]
+            firstname_score = score(firstname)
+        except:
+            firstname = ""
+            firstname_score = 0
+
+        try:
+            middlename = request.form["middlename"]
+            middlename_score = score(middlename)
+        except:
+            middlename = ""
+            middlename_score = 0
+
+        try:
+            lastname = request.form["lastname"]
+            lastname_score = score(lastname)
+        except:
+            lastname = ""
+            lastname_score = 0
+
+        try:
+            birthday = request.form["bday"]
+            birthday = birthday.replace("-", "").replace("/", "")
+            bday = birthday[:2]
+            bday_score = mini_score(bday)
+            birthday_score = mini_score(birthday)
+        except:
+            bday_score = 0
+            birthday_score = 0
+
+        final_score = score(firstname + middlename + lastname)
     return render_template("results.html", **locals())
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host="0.0.0.0", port=8080)
